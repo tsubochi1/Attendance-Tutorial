411ac124d308546b834bc0b31ace87cae9aedc9e
T_TSUBOKAWA <tsubochi11@gmail.com>
2019-11-12 00:43:09 +0000

    Finish modeling-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true                    
+                    uniqueness: true    
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
