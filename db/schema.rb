e7d20c91f45d35b2689c7cb996b4ccf7187766bf
T_TSUBOKAWA <tsubochi11@gmail.com>
2019-12-05 07:40:32 +0000

    9.3.1モーダルウインドウを表示しよう終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191130012227) do
+ActiveRecord::Schema.define(version: 20191203051342) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20191130012227) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "department"
+    t.datetime "basic_time", default: "2019-12-02 23:00:00"
+    t.datetime "work_time", default: "2019-12-02 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
